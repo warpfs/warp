1ba39f415f8558f6605786afcb519f432c413c20
Putta Khunchalee <ultimaweapon@outlook.com>
2024-04-29 00:52:38 +0700

    Fixes clippy errors

--- build.rs
+++ build.rs
@@ -1,14 +1,11 @@
 fn main() {
-    match std::env::var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
-        "macos" => {
-            println!("cargo::rustc-link-lib=framework=CoreFoundation");
-            println!("cargo::rustc-link-lib=framework=Security");
-            println!("cargo::rerun-if-changed=src/key/store/default.m");
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "macos" {
+        println!("cargo::rustc-link-lib=framework=CoreFoundation");
+        println!("cargo::rustc-link-lib=framework=Security");
+        println!("cargo::rerun-if-changed=src/key/store/default.m");
 
-            cc::Build::new()
-                .file("src/key/store/default.m")
-                .compile("warpffi")
-        }
-        _ => {}
+        cc::Build::new()
+            .file("src/key/store/default.m")
+            .compile("warpffi")
     }
 }
